5f982a24ab04ee022f02eb2d7b3c962a5c34c3e7
Kawann <kawannmarcelo7@gmail.com>
2025-04-03 10:16:55 -0300

    first commit

---
+++ src/test/java/dev/events/eventos/dev/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.events.eventos.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
