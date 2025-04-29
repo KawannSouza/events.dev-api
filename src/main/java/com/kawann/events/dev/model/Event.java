69286ec56edf8a2fe758c6ed91297068a9251adb
Kawann <kawannmarcelo7@gmail.com>
2025-04-29 15:17:53 -0300

    feat: add Evento entity with basic fields and validations

---
+++ src/main/java/com/kawann/events/dev/model/Event.java
@@ -0,0 +1,27 @@
+package com.kawann.events.dev.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotNull
+    private LocalDate date;
+}
