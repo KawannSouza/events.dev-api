8104fe22f2f6de3425bf96fa9f5f644461b41d48
Kawann <kawannmarcelo7@gmail.com>
2025-08-07 20:17:07 -0300

    feat: event creation route

---
+++ src/main/java/com/kawann/events/dev/controller/EventController.java
@@ -0,0 +1,29 @@
+package com.kawann.events.dev.controller;
+
+import com.kawann.events.dev.dto.EventRequestDTO;
+import com.kawann.events.dev.dto.EventResponseDTO;
+import com.kawann.events.dev.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+
+    private final EventService eventService;
+
+    @Autowired
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @PostMapping
+    public ResponseEntity<EventResponseDTO> createEvent(@RequestBody EventRequestDTO eventDTO) {
+        EventResponseDTO newEvent = eventService.createEvent(eventDTO);
+        return new ResponseEntity<>(newEvent, HttpStatus.CREATED);
+    }
+}
